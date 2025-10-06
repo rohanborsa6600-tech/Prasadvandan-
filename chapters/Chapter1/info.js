418dd7976699ead143fa6327e82891df2ff3eea1
rohanborsa6600-tech <rohanborsa6600@gmail.com>
2025-10-07 00:38:44 +0530

    Delete chapters/Chapter1 directory

--- chapters/Chapter1/info.js
+++
@@ -1,4 +0,0 @@
-{
-    "title": "Chapter 1: The Essence of Prasad Vandan",
-    "description": "An introduction to the devotional practice in Mahanubhav Panth, exploring the sacred offering of praise through chants and rituals. This chapter sets the spiritual foundation."
-}
